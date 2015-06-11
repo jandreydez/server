c7cc712d3153f04991636a2eb82ed53016763e6f
Allan Ferreira <allan.less@outlook.com>
2015-06-11 17:18:19 -0300

    Json PHPOO

---
+++ allan/phpoo/index.php
@@ -0,0 +1,46 @@
+<?php
+error_reporting(E_ALL);
+ini_set('desplay', 1);
+
+class JsonDB {
+
+	protected $filename;
+	protected $json;
+	
+	function __construct($filename)
+	{
+		$this->filename = $filename;	
+		$this->json = json_decode(file_get_contents($filename), true);	
+	}
+
+	function add($key, $value)
+	{
+		$this->json[$key] = $value;
+	}
+
+	function get($key)
+	{
+		return $this->json[$key];
+	}
+
+	function save()
+	{
+		return file_put_contents($this->filename, json_encode($this->json));
+	}
+}
+
+$db = new JsonDB('db.txt');
+//$db->add('3', ['name' => 'Allan Ferreira', 'email' => 'allan.less@outlook.com', 'senha' => 'UGgjhg¨hJKLGguopuyY']);
+//$db->save();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php echo $db->get(1);?>
+</body>
+</html>
