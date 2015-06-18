868d0a128e728e3165a38da5d8ec72e078c7f3a1
Allan Ferreira <allan.less@outlook.com>
2015-06-18 18:44:17 -0300

    Estudo MVC

---
+++ allan/mvc/application/views/teste.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title></title>
+        
+        <!--styles-->
+	      <link href="<?php echo asset_url();?>/css/base.css" rel="stylesheet">
+
+        <!--[if lt IE 9]>
+          	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <h1>teste</h1>
+
+        <!--scripts-->
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/vendor/jquery-1.11.0.min.js"></script>
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/vendor/jquery-migrate-1.2.1.min.js"></script>
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/vendor/jquery-ui.min.js"></script>
+      	
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/vendor/bootstrap.min.js"></script>
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/vendor/owl.carousel.min.js"></script>
+
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/plugins.js"></script>
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/functions.js"></script>
+      	<script type="text/javascript" src="<?php echo asset_url();?>/js/main.js"></script>
+    </body>
+</html>
