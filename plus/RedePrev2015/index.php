36e2017f3577af5d928530e4b2d7665fd501d1eb
jandreydez@hotmail.com <aujdb435>
2015-07-06 13:16:15 -0300

    Att

---
+++ plus/RedePrev2015/index.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <title>Redeprev</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/vendor/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">  
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="menu">
+            <li><img src="assets/images/logo.png" alt=""></li>
+            <li><a href="#">SOBRE NÓS</a></li>
+            <li><a href="#">PLANOS</a></li>
+            <li><a href="#">RESULTADOS</a></li>
+            <li><a href="#">EDUCAPREV</a></li>
+            <li><a href="#">NOTÍCIAS</a></li>
+            <li><a href="#">CONTATO</a></li>
+            <li><a href="#"><img src="assets/images/lock.png" alt="">HomePrev</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="banner"></div>
+    <div class="bar">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <ul>
+              <li>
+                <a href="#">
+                  <img src="assets/images/pig.png" alt="">
+                  <h2>EDUCAPREV</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing dolor.</p>
+                </a>
+              </li>                
+              <li>
+                <a href="#">
+                  <img src="assets/images/result.png" alt="">
+                  <h2>RESULTADOS</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing dolor.</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="assets/images/carteira.png" alt="">
+                  <h2>EMPRÉSTIMOS</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing dolor.</p>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="assets/images/folha.png" alt="">
+                  <h2>REDEPREV NOTÍCIAS</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing dolor.</p>
+                </a> 
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container bar2">
+      <div class="row">
+        <div class="col-md-6 col-md-offset-1">
+          <iframe src="https://www.youtube.com/embed/NBOsvhCjviY" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <div class="col-md-4">
+          <h2>REDEPREV</h2>
+          <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati ullamco laboris nisi ut aliquip ex ea commod exercitation ullam laboris nisi ut aliquip ex ea commod. exercitation ullamco laboris nisi ut aliquip ex ea commod.</p>
+
+          <p >Ut enim ad minim veniam, quis nostrud exercitati ullamco laboris nisi ut aliquip ex ea commod</p>
+          <a href="#">
+            <span>Saiba mais sobre RedePrev</span>
+          </a>          
+        </div>
+      </div>
+    </div>
+    <div class="container bar3">
+      <div class="row">
+        <div class="col-md-10 col-sm-10 col-md-offset-1 ">
+          <h2>Notícias</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 col-sm-5 col-md-offset-1  noticia-principal">
+          <a href="#">
+            <figure>
+            <img src="assets/images/familianoticias.jpg" alt="">
+            <figcaption><h2>28</h2><p>jun/2015</p></figcaption>            
+            </figure>
+            <h2>Titulo da notícia Titulo da notícia</h2>
+            <p>Lorem ipsum dolor sit amet, consect adipiscing elit, sed do eiusmod tem dolor sit amet.</p>
+          </a>
+        </div>
+        <div class="col-md-7 col-sm-7">
+          <div class="col-md-12 col-sm-12">
+            <div class="row"> 
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+              <div class="col-md-6 col-sm-6 sub-noticias">      
+                    <a href="#">
+                      <img src="assets/images/subnoticia.jpg" alt="">
+                      <h2>Titulo da Notícia</h2>
+                      <p>Lorem ipsum dolor sit amet, consect </p>
+                    </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="col-md-3">
+                  <h2>TITULO</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati ullamco laboris nisi ut aliquip ex ea commod exercitation ullam laboris nisi ut aliquip ex ea commod. exercitation ullamco laboris nisi ut aliquip ex ea commod.</p>
+                </div>
+                <div class="col-md-3">
+                   <h2>TITULO</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati ullamco laboris nisi ut aliquip ex ea commod exercitation ullam laboris nisi ut aliquip ex ea commod. exercitation ullamco laboris nisi ut aliquip ex ea commod.</p>
+                </div>
+                <div class="col-md-3">
+                   <h2>TITULO</h2>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati ullamco laboris nisi ut aliquip ex ea commod exercitation ullam laboris nisi ut aliquip ex ea commod. exercitation ullamco laboris nisi ut aliquip ex ea commod.</p>
+                </div>
+                <div class="col-md-3">
+                  <h2>CONTATOS</h2>
+                  <ul>
+                    <li><span class="tel"></span>2222-2222 | 3333-3333</li>
+                    <li><span class="msg"></span>contato@redeprev.com</li>
+                    <li><span class="loc"></span>Av. Presidente Wilson,165 Sala 712 - Centro Rio de Janeiro
+                    </li>
+                  </ul>
+
+                </div>
+              </div>
+            </div>            
+          </div>
+        </div>
+      </div>      
+    </div>
+    <div class="energisa">        
+        <img src="assets/images/energisa.png" alt="">      
+     </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
--- plus/estudo_sebrae/wwwroot/wp-content/themes/sebraeprev/single-fotos.php
+++ plus/estudo_sebrae/wwwroot/wp-content/themes/sebraeprev/single-fotos.php
@@ -5,23 +5,20 @@
             <h2><?php the_title();?></h2>
             <?php the_content();?>
             <div class="owl-carousel">
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
-			    <div class="item"><img src="http://wallpapernorth.com/wp-content/uploads/cool-Cat-wallpaper1_1_1xbpByg.jpg" alt=""></div>
+                <div class="item"><img src="http://2.bp.blogspot.com/_EZ16vWYvHHg/S-Bl2fuyyWI/AAAAAAAAMKc/DNayYJK8mEo/s1600/www.BancodeImagenesGratuitas.com-Fantasticas-20.jpg" alt="" /></div>            
+			    <div class="item"><img src="http://2.bp.blogspot.com/_EZ16vWYvHHg/S-Bl2fuyyWI/AAAAAAAAMKc/DNayYJK8mEo/s1600/www.BancodeImagenesGratuitas.com-Fantasticas-20.jpg" alt="" /></div>		    
+			    
     	    </div>
         </div>
     </div>
 </div>
 <script>
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    items:1    
-});
+        items:1,
+        loop:false,
+        center:true,
+        margin:10,
+        
+    });
 </script>
 <?php endwhile; get_footer();?>
